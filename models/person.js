dba24c7703c66db45773a398e6658ea85d8db4ac
manuaba42 <ssedanayoga3@gmail.com>
2026-07-30 11:34:24 +0800

    Phonebook database, step 7 validation

--- models/person.js
+++ models/person.js
@@ -18,7 +18,11 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 })
 
